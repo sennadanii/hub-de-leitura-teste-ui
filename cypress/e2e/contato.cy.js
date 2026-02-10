27caf4861ac1cbc6eebf40eacf6de26ef47187b8
Danielle Senna <sennadanii@live.com>
2026-02-09 21:54:49 -0300

    Automação do contato

---
+++ cypress/e2e/contato.cy.js
@@ -0,0 +1,55 @@
+describe('Funcionalidade: Contato', () => {
+
+  beforeEach(() => {
+    cy.visit('index.html')
+  });
+  
+  it('Deve preencher formulário de contato com sucesso', () => {
+    cy.get('[name="name"]').type('Danielle Senna')
+    cy.get('[name="email"]').type('senna@teste.com')
+    cy.get('[name="subject"]').select('Parcerias')
+    cy.get('[name="message"]').type('Mensagem de teste')
+    cy.get('#btn-submit').click()
+    //Resultado esperado
+    cy.contains('Contato enviado com sucesso!').should('exist')
+  });
+
+  it('Deve validar mensagem de erro ao enviar sem preencher nome', () => {
+    cy.get('[name="name"]').clear()
+    cy.get('[name="email"]').type('senna@teste.com')
+    cy.get('[name="subject"]').select('Parcerias')
+    cy.get('[name="message"]').type('Mensagem de teste')
+    cy.get('#btn-submit').click()
+    //Resultado esperado
+    cy.get('#alert-container').should('contain', 'Por favor, preencha o campo Nome')
+  });
+
+  it('Deve validar mensagem de erro ao enviar sem preencher email', () => {
+    cy.get('[name="name"]').type('Danielle Senna')
+    cy.get('[name="email"]').clear()
+    cy.get('[name="subject"]').select('Parcerias')
+    cy.get('[name="message"]').type('Mensagem de teste')
+    cy.get('#btn-submit').click()
+    //Resultado Esperado
+    cy.get('#alert-container').should('contain', 'Por favor, preencha o campo E-mail')
+  });
+
+  it('Deve validar mensagem de erro ao enviar sem selecionar o assunto', () => {
+    cy.get('[name="name"]').type('Danielle Senna')
+    cy.get('[name="email"]').type('senna@teste.com')
+    cy.get('[name="message"]').type('Mensagem de teste')
+    cy.get('#btn-submit').click()
+    //Resultado Esperado
+    cy.get('#alert-container').should('contain', 'Por favor, selecione o Assunto')
+     });
+
+    it('Deve validar mensagem de erro ao enviar sem preencher a mensagem', () => {
+    cy.get('[name="name"]').type('Danielle Senna')
+    cy.get('[name="email"]').type('senna@teste.com')
+    cy.get('[name="subject"]').select('Parcerias')
+    cy.get('[name="message"]').clear()
+    cy.get('#btn-submit').click()
+    //Resultado Esperado
+    cy.get('#alert-container').should('contain', 'Por favor, escreva sua Mensagem')
+    });
+  });
